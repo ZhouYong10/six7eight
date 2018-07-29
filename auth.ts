0f5f889193dcf91ec06de72ac528cf7ba645a6fd
zhouyong10 <1059981087@qq.com>
2018-07-29 01:01:40 -0700

    完成从数据库验证用户登录

--- auth.ts
+++ auth.ts
@@ -1,41 +1,62 @@
 import passport = require('koa-passport');
 import PassportLocal = require('passport-local');
+import {getRepository} from "typeorm";
+import {User} from "./entity/User";
+import {UserSite} from "./entity/UserSite";
+import {UserAdmin} from "./entity/UserAdmin";
+import {comparePass} from "./utils";
 
 const LocalStrategy = PassportLocal.Strategy;
 
 
-const fetchUser = (() => {
-    const user:any = { id: 1, username: 'test', password: 'test' };
-    return async () => {
-        return user;
+async function fetchUserByName(username: string) {
+    let user:any = await getRepository(User).findOne({username: username});
+    if (!user) {
+        user = await getRepository(UserSite).findOne({username: username});
     }
-})();
+    if (!user) {
+        user = await getRepository(UserAdmin).findOne({username: username});
+    }
+    return user;
+}
+
+async function fetchUserById(id: string) {
+    let user: any = await getRepository(User).findOne(id);
+    if (!user) {
+        user = await getRepository(UserSite).findOne(id);
+    }
+    if (!user) {
+        user = await getRepository(UserAdmin).findOne(id);
+    }
+    return user;
+}
 
 passport.serializeUser((user:any, done) => {
     console.log('serializeUser: ' + JSON.stringify(user));
     done(null, user.id)
 });
 
-passport.deserializeUser(async (id, done) => {
+passport.deserializeUser(async (id:string, done) => {
     console.log('deserializeUser: ' + id);
     try {
-        const user = await fetchUser();
+        const user = await fetchUserById(id);
         done(null, user);
     } catch(err) {
         done(err);
     }
 });
 
-passport.use(new LocalStrategy((username, password, done) => {
+passport.use(new LocalStrategy(async (username, password, done) => {
     console.log('LocalStrategy username: ' + username);
     console.log('LocalStrategy password: ' + password);
-    fetchUser()
-        .then(user => {
-            if (username === user.username && password === user.password) {
-                done(null, user);
-            } else {
-                done(null, false);
-            }
-        })
-        .catch((err) => { done(err) });
+    try{
+        let user = await fetchUserByName(username);
+        if (user && user.username === username && comparePass(password, user.password)) {
+            done(null, user);
+        } else {
+            done(null, false);
+        }
+    }catch (e) {
+        done(e);
+    }
 }));
--- utils.ts
+++ utils.ts
@@ -1,4 +1,5 @@
 import {Context} from "koa";
+import * as bcrypt from "bcryptjs";
 
 export function authenticated(todo:(ctx: Context, next?: any) => any){
     return async (ctx: Context, next?: any) => {
@@ -8,4 +9,8 @@ export function authenticated(todo:(ctx: Context, next?: any) => any){
             ctx.body = '您还没有登录，请登录后在操作！！';
         }
     }
+}
+
+export function comparePass(userPass:string, databasePass:string) {
+    return bcrypt.compareSync(userPass, databasePass);
 }
