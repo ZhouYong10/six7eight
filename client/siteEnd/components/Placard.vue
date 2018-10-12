bef752a5edfc22bc9149848e7686ea966b706cae
zhouyong10 <1059981087@qq.com>
2018-10-12 13:02:27 +0800

    添加分站公告管理页面

--- client/siteEnd/components/Placard.vue
+++ client/siteEnd/components/Placard.vue
@@ -1,13 +1,176 @@
 <template>
-    <h1>分站公告管理 ....</h1>
+    <div style="height: 100%">
+
+        <el-row type="flex" justify="end">
+            <el-col style="text-align: right; padding-right: 50px;">
+                <el-button type="success" icon="el-icon-circle-plus-outline"
+                           @click="dialogVisible = true">添 加</el-button>
+            </el-col>
+        </el-row>
+
+        <el-table
+                :data="tableData"
+                :row-class-name="tableRowClassName"
+                height="93%">
+            <el-table-column
+                    label="创建日期"
+                    min-width="180">
+                <template slot-scope="scope">
+                    <i class="el-icon-time" style="color: #ff2525"></i>
+                    <span>{{ scope.row.createTime}}</span>
+                </template>
+            </el-table-column>
+            <el-table-column
+                    prop="name"
+                    label="名称"
+                    min-width="160">
+            </el-table-column>
+            <el-table-column
+                    label="状态"
+                    min-width="300">
+                <template slot-scope="scope">
+                    {{ scope.row.onSale ? '上架' : '下架'}}
+                </template>
+            </el-table-column>
+            <el-table-column
+                    label="操作"
+                    width="188">
+                <template slot-scope="scope">
+                    <el-button type="primary" plain icon="el-icon-edit" size="small" @click="edit(scope.row)">编 辑</el-button>
+                    <el-button type="danger" plain icon="el-icon-delete" size="small" @click="remove(scope.row.id)">删 除</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+
+        <el-dialog :title="dialogTitle" :visible.sync="dialogVisible" top="6vh" width="30%" @closed="cancelDialog">
+            <el-form :model="dialog" :rules="rules" ref="dialog" :label-width="dialogLabelWidth">
+                <el-form-item label="名称" prop="name">
+                    <el-input v-model="dialog.name" auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="状态" >
+                    <el-switch
+                            v-model="dialog.onSale"
+                            active-text="上架"
+                            inactive-text="下架">
+                    </el-switch>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisible = false">取 消</el-button>
+                <el-button v-if="!dialog.edit" type="primary" @click="add">确 定</el-button>
+                <el-button v-if="dialog.edit" type="primary" @click="update">保 存</el-button>
+            </div>
+        </el-dialog>
+    </div>
 </template>
 
 <script>
+    import {axiosGet, axiosPost} from "@/utils";
+
     export default {
-        name: "NoPage"
+        name: "Placard",
+        async created() {
+            this.tableData = await axiosGet('/site/auth/product/types');
+        },
+        data() {
+            return {
+                tableData: [],
+                dialogLabelWidth: '60px',
+                dialogVisible: false,
+                dialogTitle: '添加商品类别',
+                dialog: {
+                    name: '',
+                    onSale: true
+                },
+                rules: {
+                    name: [
+                        {required: true, message: '请输入商品类别名称!', trigger: 'blur'},
+                        { validator: async (rule, value, callback) => {
+                                let oldName;
+                                if (this.dialog.type) {
+                                    oldName = this.dialog.type.name;
+                                }
+                                if (value !== oldName) {
+                                    let type = await axiosGet('/site/auth/product/type/' + value + '/exist');
+                                    if (type) {
+                                        callback(new Error('商品类别: ' + value + ' 已经存在！'));
+                                    } else {
+                                        callback();
+                                    }
+                                }else{
+                                    callback();
+                                }
+                            }, trigger: 'blur'}
+                    ]
+                }
+            }
+        },
+        methods: {
+            tableRowClassName({row}) {
+                return row.onSale ? 'for-sale' : 'not-sale';
+            },
+            cancelDialog() {
+                this.dialogTitle = "添加商品类别";
+                this.$refs.dialog.resetFields();
+            },
+            add() {
+                this.$refs.dialog.validate(async (valid) => {
+                    if (valid) {
+                        let type = await axiosPost('/site/auth/product/type/add', this.dialog);
+                        this.tableData.unshift(type);
+                        this.dialogVisible = false;
+                    } else {
+                        return false;
+                    }
+                });
+            },
+            edit(type) {
+                this.dialogTitle = '编辑商品类别';
+                this.dialog = {
+                    id: type.id,
+                    name: type.name,
+                    onSale: type.onSale,
+                    type: type,
+                    edit: true
+                };
+                this.dialogVisible = true;
+            },
+            update() {
+                this.$refs.dialog.validate(async (valid) => {
+                    if (valid) {
+                        let updatedType = await axiosPost('/site/auth/product/type/update', this.dialog);
+                        this.dialog.type.name = updatedType.name;
+                        this.dialog.type.onSale = updatedType.onSale;
+                        this.dialogVisible = false;
+                    } else {
+                        return false;
+                    }
+                });
+            },
+            remove(id) {
+                this.$confirm('此操作将永久删除所选角色！', '注意', {
+                    confirmButtonText: '确 定',
+                    cancelButtonText: '取 消',
+                    type: 'warning'
+                }).then(async () => {
+                    await axiosGet('/site/auth/role/remove/' + id);
+                    this.tableData = this.tableData.filter((val) => {
+                        return val.id !== id;
+                    });
+                }).catch((e) => {
+                    console.log(e);
+                });
+            }
+        }
     }
 </script>
 
-<style scoped>
+<style lang="scss">
+    .el-table .for-sale {
+        background: #F0F9EB;
+    }
 
+    .el-table .not-sale {
+        background: #FEF0F0;
+    }
 </style>
