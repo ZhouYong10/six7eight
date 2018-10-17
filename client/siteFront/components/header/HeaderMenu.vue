d909eef00ac582c6426705ea5ff042cd2f77fd13
ZhouYong10 <1059981087@qq.com>
2018-10-17 14:23:35 +0800

    修复前端登录弹窗的样式影响其他弹窗的问题

--- client/siteFront/components/header/HeaderMenu.vue
+++ client/siteFront/components/header/HeaderMenu.vue
@@ -188,102 +188,102 @@
                 cursor: pointer;
             }
         }
-    }
 
-    .el-dialog__header {
-        padding: 0;
-        button{
-            z-index: 100;
-            .el-dialog__close{
-                color: white;
+        .el-dialog__header {
+            padding: 0;
+            button{
+                z-index: 100;
+                .el-dialog__close{
+                    color: white;
+                }
+                .el-dialog__close:hover{
+                    color: #000;
+                }
             }
-            .el-dialog__close:hover{
-                color: #000;
+        }
+        .el-dialog__body{
+            padding: 0;
+            height: inherit;
+        }
+        @keyframes animate-cloud{
+            from {
+                background-position: 600px 100%;
+            }
+            to {
+                background-position: 0 100%;
             }
         }
-    }
-    .el-dialog__body{
-        padding: 0;
-        height: inherit;
-    }
-    @keyframes animate-cloud{
-        from {
-            background-position: 600px 100%;
+        .wrapper{
+            background:url(cloud.jpg) 0 bottom repeat-x  #049ec4;
+            animation: animate-cloud 20s linear infinite;
+            width: auto;
+            height: 100%;
+            position: relative;
+            z-index: 0;
         }
-        to {
-            background-position: 0 100%;
+        #content {
+            background: #f9f9f9;
+            background: linear-gradient(top,  rgba(248,248,248,1) 0%,rgba(249,249,249,1) 100%);
+            box-shadow: 0 1px 0 #fff inset;
+            border: 1px solid #c4c6ca;
+            margin: 0 auto;
+            padding: 25px 0 0;
+            position: relative;
+            top: 136px;
+            text-align: center;
+            text-shadow: 0 1px 0 #fff;
+            width: 400px;
+        }
+        #content h1 {
+            color: #7E7E7E;
+            font: bold 25px Helvetica, Arial, sans-serif;
+            letter-spacing: -0.05em;
+            line-height: 20px;
+            margin: 0 0 30px;
+        }
+        #content h1:before,
+        #content h1:after {
+            content: "";
+            height: 1px;
+            position: absolute;
+            top: 36px;
+            width: 27%;
+        }
+        #content h1:after {
+            background: rgb(126,126,126);
+            background: linear-gradient(left,  rgba(126,126,126,1) 0%,rgba(255,255,255,1) 100%);
+            right: 12px;
+        }
+        #content h1:before {
+            background: rgb(126,126,126);
+            background: linear-gradient(right,  rgba(126,126,126,1) 0%,rgba(255,255,255,1) 100%);
+            left: 12px;
+        }
+        #content:after,
+        #content:before {
+            background: #f9f9f9;
+            background: linear-gradient(top,  rgba(248,248,248,1) 0%,rgba(249,249,249,1) 100%);
+            border: 1px solid #c4c6ca;
+            content: "";
+            display: block;
+            height: 100%;
+            left: -1px;
+            position: absolute;
+            width: 100%;
+        }
+        #content:after {
+            transform: rotate(3deg);
+            top: 0;
+            z-index: -1;
+        }
+        #content:before {
+            transform: rotate(-3deg);
+            top: 0;
+            z-index: -1;
+        }
+        #content form {
+            padding: 0 30px 0 0;
         }
     }
-    .wrapper{
-        background:url(cloud.jpg) 0 bottom repeat-x  #049ec4;
-        animation: animate-cloud 20s linear infinite;
-        width: auto;
-        height: 100%;
-        position: relative;
-        z-index: 0;
-    }
-    #content {
-        background: #f9f9f9;
-        background: linear-gradient(top,  rgba(248,248,248,1) 0%,rgba(249,249,249,1) 100%);
-        box-shadow: 0 1px 0 #fff inset;
-        border: 1px solid #c4c6ca;
-        margin: 0 auto;
-        padding: 25px 0 0;
-        position: relative;
-        top: 136px;
-        text-align: center;
-        text-shadow: 0 1px 0 #fff;
-        width: 400px;
-    }
-    #content h1 {
-        color: #7E7E7E;
-        font: bold 25px Helvetica, Arial, sans-serif;
-        letter-spacing: -0.05em;
-        line-height: 20px;
-        margin: 0 0 30px;
-    }
-    #content h1:before,
-    #content h1:after {
-        content: "";
-        height: 1px;
-        position: absolute;
-        top: 36px;
-        width: 27%;
-    }
-    #content h1:after {
-        background: rgb(126,126,126);
-        background: linear-gradient(left,  rgba(126,126,126,1) 0%,rgba(255,255,255,1) 100%);
-        right: 12px;
-    }
-    #content h1:before {
-        background: rgb(126,126,126);
-        background: linear-gradient(right,  rgba(126,126,126,1) 0%,rgba(255,255,255,1) 100%);
-        left: 12px;
-    }
-    #content:after,
-    #content:before {
-        background: #f9f9f9;
-        background: linear-gradient(top,  rgba(248,248,248,1) 0%,rgba(249,249,249,1) 100%);
-        border: 1px solid #c4c6ca;
-        content: "";
-        display: block;
-        height: 100%;
-        left: -1px;
-        position: absolute;
-        width: 100%;
-    }
-    #content:after {
-        transform: rotate(3deg);
-        top: 0;
-        z-index: -1;
-    }
-    #content:before {
-        transform: rotate(-3deg);
-        top: 0;
-        z-index: -1;
-    }
-    #content form {
-        padding: 0 30px 0 0;
-    }
-
+    
 </style>
